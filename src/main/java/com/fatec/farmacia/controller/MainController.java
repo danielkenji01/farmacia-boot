323ea8a14bb1a5e845516273025ddf2d772a88b0
danielkenji01 <daniel_kenji_1@hotmail.com>
2021-06-04 07:46:19 -0300

    ajuste na controller principal

--- src/main/java/com/fatec/farmacia/controller/MainController.java
+++ src/main/java/com/fatec/farmacia/controller/MainController.java
@@ -1,17 +1,13 @@
 package com.fatec.farmacia.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/app")
-    public String main(Model model) {
-
-        model.addAttribute("teste", "Daniel");
-
+    @GetMapping({"/app", ""})
+    public String main() {
         return "app";
     }
 
