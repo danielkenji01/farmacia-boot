67b75fa1b061e17dcd4e5377991571dadcf32f45
danielkenji01 <daniel_kenji_1@hotmail.com>
2021-06-04 08:23:28 -0300

    busca de fornecedores não excluídos

--- src/main/java/com/fatec/farmacia/repository/FornecedorRepository.java
+++ src/main/java/com/fatec/farmacia/repository/FornecedorRepository.java
@@ -3,5 +3,10 @@ package com.fatec.farmacia.repository;
 import com.fatec.farmacia.model.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+
+    List<Fornecedor> findAllByDataExclusaoIsNull();
+
 }
